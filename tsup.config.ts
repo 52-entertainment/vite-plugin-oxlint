8fd57ef4748e8e50f19b8bbcc76ccad9ea6cd428
Arnaud Riu <arnaud.riu@52-entertainment.com>
2026-03-02 14:39:59 +0100

    Switch build to tsup, clean up rename script

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm', 'cjs'],
+  dts: true,
+  clean: true,
+  outExtension({ format }) {
+    return { js: format === 'esm' ? '.mjs' : '.cjs' }
+  }
+})
